b4df6abade02af4664adaf8f4226fc7f9efa7c53
Michael Hopkins <mhopkins321@gmail.com>
2016-05-04 21:14:36 -0500

    fifth

--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -3,9 +3,5 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <title>{!! $title or 'No Title Set' !!}</title>
-<link href="/assets/css/docs.css" rel="stylesheet">
-<link href="/assets/css/bootstrap.css" rel="stylesheet">
-<link href="/assets/css/bootstrap-responsive.css" rel="stylesheet">
-<link href="/assets/css/prettify.css" rel="stylesheet">
-<link href="/assets/css/offcanvas.css" rel="stylesheet">
+<link href="/css/all.css" rel="stylesheet">
 @yield('styles')
